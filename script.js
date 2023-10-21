ac2a266d78d6b86f37422768eb16a4484d744a92
Matheus-hfv <119900495+Matheus-hfv@users.noreply.github.com>
2023-10-21 14:47:17 -0300

    teste

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  function mostrarAlertacad(){
+    var nomeCadastro = document.getElementById('username').value;
+    alert(`Cadastro de ${nomeCadastro} bem-sucedido!`);
+  }
+
+  
+function mostrarAlerta() {
+    var nome = document.getElementById('nome').value;
+    
+      alert(`Login de ${nomes} bem-sucedido!`);
+    
+  };
+
+
+  
